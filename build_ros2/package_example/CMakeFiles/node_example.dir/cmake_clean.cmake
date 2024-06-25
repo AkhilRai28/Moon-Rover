3961d0fe4f651c62d8fe01bfc9d19dd4200b00ee
Akhil <144369179+AkhilRai28@users.noreply.github.com>
2024-06-25 15:08:19 +0530

    ros2

---
+++ build_ros2/package_example/CMakeFiles/node_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_example.dir/src/node_example.cpp.o"
+  "CMakeFiles/node_example.dir/src/node_example.cpp.o.d"
+  "node_example"
+  "node_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_ros2/package_example/CMakeFiles/node_example.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for node_example.
+# This may be replaced when dependencies are built.
